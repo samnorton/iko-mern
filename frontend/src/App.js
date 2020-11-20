234da5de462f01c81b1ab1561ec2844ecd872df1
Sam Norton <samuelnico.norton@gmail.com>
2020-11-20 08:36:35 +0800

    Added react-router-dom to the setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen'
 import ResortDetail from './screens/ResortDetail'
 import Header from './components/Header'
@@ -6,11 +7,14 @@ import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <> 
+    <Router> 
        <Header />
-
+          <main>
+             <Route path='/' component={HomeScreen} exact />
+             <Route path='/resort' component={ResortDetail} />
+          </main>
        <Footer />
-    </>
+    </Router>
   )
 }
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,20 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="/">Navbar</a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="#">Home</a>
-        <a className="nav-link" href="#">Features</a>
-        <a className="nav-link" href="#">Pricing</a>
-      
+        <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+        <Link className="nav-link" to="/resort">Resort Detail</Link>
+        <Link className="nav-link" to="/">Contact</Link>
       </div>
     </div>
   </div>
